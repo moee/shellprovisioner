bab0b9560caf214f9abe690881652e515ec1344a
moee <moee@github>
2014-09-17 11:31:40 +0200

    Export path variables
    So we can access the paths within the scripts

--- provision.sh
+++ provision.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 config=$1
-configs_dir=configs
-tasks_dir=tasks
+
+export ZDSP_BASE_DIR="$( cd "$( dirname "$0" )" && pwd )"
+export ZDSP_TASK_DIR=$ZDSP_BASE_DIR/tasks
+export ZDSP_CONFIG_DIR=$ZDSP_BASE_DIR/configs
 
 success () { echo -e "\e[00;32mSuccess\e[00m"; }
 error () { echo -e "\e[00;31mError: $1\e[00m"; }
@@ -18,36 +20,36 @@ if [ -z $config ]; then
     exit 1
 fi
 
-if [ ! -f $configs_dir/$config ]; then
+if [ ! -f $ZDSP_CONFIG_DIR/$config ]; then
     fatal "Config $config does not exist."
 fi
 
 info "Running config $config"
 
 info "Checking tasks"
-for task in `cat $configs_dir/$config | sed $'s/\r$//'`; do
-    if [ ! -d $tasks_dir/$task ]; then
-        fatal "task directory $tasks_dir/$task not found" 
+for task in `cat $ZDSP_CONFIG_DIR/$config | sed $'s/\r$//'`; do
+    if [ ! -d $ZDSP_TASK_DIR/$task ]; then
+        fatal "task directory $ZDSP_TASK_DIR/$task not found" 
     fi
 
-    if [ ! -f $tasks_dir/$task/do.sh ]; then
-        fatal "task file $tasks_dir/$task/do.sh not found" 
+    if [ ! -f $ZDSP_TASK_DIR/$task/do.sh ]; then
+        fatal "task file $ZDSP_TASK_DIR/$task/do.sh not found" 
     fi
 done
 
 info "Running tasks"
 num=1 
-for task in `cat $configs_dir/$config | sed $'s/\r$//'`; do
+for task in `cat $ZDSP_CONFIG_DIR/$config | sed $'s/\r$//'`; do
     info "starting task $num: $task"
-    sed -i $'s/\r$//' $tasks_dir/$task/do.sh
-    bash $tasks_dir/$task/do.sh
+    sed -i $'s/\r$//' $ZDSP_TASK_DIR/$task/do.sh
+    bash $ZDSP_TASK_DIR/$task/do.sh
     if [ $? != 0 ]; then
         error "task $task failed. Rolling back."
-        for task in `tac $configs_dir/$config | tail -n $num`; do
-            if [ -f $tasks_dir/$task/undo.sh ]; then
+        for task in `tac $ZDSP_CONFIG_DIR/$config | tail -n $num`; do
+            if [ -f $ZDSP_TASK_DIR/$task/undo.sh ]; then
                 info "undo $task"
-                sed -i $'s/\r$//' $tasks_dir/$task/undo.sh
-                bash $tasks_dir/$task/undo.sh            
+                sed -i $'s/\r$//' $ZDSP_TASK_DIR/$task/undo.sh
+                bash $ZDSP_TASK_DIR/$task/undo.sh            
             fi
         done
         error "provisioning of $config failed."
