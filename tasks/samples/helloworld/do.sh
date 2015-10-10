5619ac44c948e5ef9deb9e67731fbf851ae2b833
moee <moee@github>
2015-10-10 15:43:19 +0200

    v0.1.1 supports config variables

--- tasks/samples/helloworld/do.sh
+++ tasks/samples/helloworld/do.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
-echo "Hello World!"
-echo "The Zero Dependecy Shell Provisioner directory is $ZDSP_BASE_DIR"
-echo "Place your configurations in $ZDSP_CONFIG_DIR and tasks into $ZDSP_TASK_DIR"
+echo $message
+zdsp_info "The Zero Dependecy Shell Provisioner directory is $ZDSP_BASE_DIR"
+zdsp_info "Place your configurations in $ZDSP_CONFIG_DIR and tasks into $ZDSP_TASK_DIR"
